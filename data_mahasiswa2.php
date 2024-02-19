4f4d191b02042cbf98ed00ce1635f6046d059da7
aptnjordan <159239731+aptnjordan@users.noreply.github.com>
2024-02-19 15:35:45 +0700

    Update data_mahasiswa2.php

---
+++ data_mahasiswa2.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>Data Mahasiswa</h1>";
+echo "NIM :22110003 <br>";
+echo "Nama Lengkap : Aprianton Jordan";
+echo "Kelas : 22 MI A";
+echo "Semester Genap";
+echo "Tahun Akademik";
+?>
